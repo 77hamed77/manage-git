3ccd7b807cedb79e037f0de4ddcee407a43d1c33
Hamid_Muhammad <h55180d@gmail.com>
2025-04-08 14:55:48 +0300

    added row js in file config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 console.log("version one");
 const body = document.querySelector("body");
 const titleClick = document.querySelector("div");
+const title = document.querySelector('h2')
 titleClick.addEventListener("click", () => {
   body.style.backgroundColor = "black";
   titleClick.style.backgroundColor = "red";
+  title.style.color = 'white'
 });
